6c1e14a7575c98c17b9dcfe9a7c933fd256e23bf
kanhaiya RD <118708023+Kana121@users.noreply.github.com>
2023-09-09 20:26:46 +0530

    my first portfolio
    my first portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+const cube = document.querySelector(".cube");
+
+cube.addEventListener("mouseover", () => {
+    cube.style.animationPlayState = "paused";
+});
+
+cube.addEventListener("mouseout", () => {
+    cube.style.animationPlayState = "running";
+});
